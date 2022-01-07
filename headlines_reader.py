0f58efbfeb742d4bef6985ee6a1a846aeb62cc76
Miroslav Kopecky <kopecky.mir@gmail.com>
2022-01-07 16:37:43 +0100

    Writes first headline based on keyword and source

--- headlines_reader.py
+++ headlines_reader.py
@@ -1,34 +1,25 @@
 #pip install newsapi-python
 from newsapi import NewsApiClient
+import os 
+
+cwd = os.getcwd()
+print(cwd)
 
 def read_api():
-    with open("Api_key.txt", "r") as apikey:
-       file = apikey.read()
-       print(file)
+    with open('Api_key.txt', 'r') as apikey:
+        file = apikey.read()
+        return file
 
 # Init
 newsapi = NewsApiClient(api_key=read_api())
 
+
 # /v2/top-headlines
-""" top_headlines = newsapi.get_top_headlines(q='bitcoin',
-                                          sources='bbc-news,the-verge',
-                                          category='business',
-                                          language='en',
-                                          country='us') """
+top_headlines = newsapi.get_top_headlines(q='Bitcoin',
+                                          sources='bbc-news,the-verge'
+                                                                    )
 
-
-# /v2/everything
-all_articles = newsapi.get_everything(q='bitcoin',
-                                      sources='bbc-news,the-verge',
-                                      domains='bbc.co.uk,techcrunch.com',
-                                      from_param='2017-12-01',
-                                      to='2017-12-12',
-                                      language='en',
-                                      sort_by='relevancy',
-                                      page=2)
-
-# /v2/top-headlines/sources
-sources = newsapi.get_sources()
-
-""" for items in all_articles['sources']:
-    print(items['name']) """
+with open('test_file.txt', 'w') as file:
+    for items in top_headlines['articles']:
+        file.write(items['source']['name'] + '\n')
+        file.write(items['title'])
