3e3c503d9da4065652e12c1599b48aa2d4ec2845
Miro <kopecky.mir@gmail.com>
2022-01-08 23:16:46 +0100

    first commit with request

--- headlines_reader.py
+++ headlines_reader.py
@@ -1,26 +1,22 @@
 # pip install newsapi-python
-# https://newsapi.org/docs 
-from newsapi import NewsApiClient
-import os 
+# https://newsapi.org/docs
+  
+import requests
+import json
 
-cwd = os.getcwd()
-print(cwd)
+from requests.api import head
 
-def read_api():
-    with open('Api_key.txt', 'r') as apikey:
-        file = apikey.read()
-        return file
-
-# Init
-newsapi = NewsApiClient(api_key=read_api())
+def api_key():
+    with open('Api_key.txt', 'r') as key:
+        key_read =key.read()
+        return key_read
 
 
-# /v2/top-headlines
-top_headlines = newsapi.get_top_headlines(q='bitcoin',
-                                          sources='bbc-news,the-verge'
-                                                                    )
+def top_headlines_query(keyword):
+    url = 'https://newsapi.org/v2/top-headlines?q=' + keyword + '&apiKey=' + api_key()
 
-with open('test_file.txt', 'w') as file:
-    for items in top_headlines['articles']:
-        file.write(items['source']['name'] + '\n')
-        file.write(items['title'])
+    return(url) 
+
+response = requests.get(url = top_headlines_query('bitcoin'))
+
+    
